2e8a77b1a6a4280edf12ea86d7b410154859643a
Daniel Cascante Vargas <cascantekevin374@gmail.com>
2022-05-20 17:47:54 -0600

    Proceso que busca producto por categoria

---
+++ SearchProduct.sql
@@ -0,0 +1,19 @@
+-- Buscar un producto por caracacteristica 
+CREATE PROCEDURE dbo.searchProduct
+@caracteristica NVARCHAR(MAX),
+@valor NVARCHAR(MAX)
+AS
+BEGIN
+	DECLARE @query NVARCHAR(MAX) = N'$.' + @caracteristica;
+	
+	SELECT
+		nombre,
+		JSON_VALUE(caract, '$.precio') AS Precio 
+	FROM Producto WHERE JSON_VALUE(caract, @query) = @valor;		
+END
+	
+DECLARE @inCaract NVARCHAR(MAX) = 'material', @inValue NVARCHAR(MAX) = 'madera';
+
+EXEC searchProduct
+	@caracteristica = @inCaract,
+	@valor = @inValue;
